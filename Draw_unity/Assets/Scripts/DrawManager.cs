87e89acd167cb4113464dd5ae81525d013eb287e
daniell <liviudaniel.lupascu@gmail.com>
2019-06-01 20:24:18 +0300

    some math working

--- Draw_unity/Assets/Scripts/DrawManager.cs
+++ Draw_unity/Assets/Scripts/DrawManager.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 
 public class DrawManager : MonoBehaviour
 {
     public GameObject drawPrefab;
-
-    public Sprite drawPlane;
+    public TMP_Text result;
 
     GameObject theTrail;
+
+    int initialPatternCount;
+    public float lineWeight = 0.22f;
+
+    public Transform linesHolder;
+    void Start()
+    {
+        initialSnapshot = true;
+    }
 
     public void Update()
     {
@@ -19,7 +25,8 @@ public class DrawManager : MonoBehaviour
             var hit = Physics2D.Raycast(mouseRay, Vector2.zero);
             if (hit.transform != null)
             {
-                theTrail = Instantiate(drawPrefab, hit.collider.transform.position + (Vector3)hit.point + Vector3.back , Quaternion.identity);
+                theTrail = Instantiate(drawPrefab, hit.collider.transform.position + (Vector3) hit.point + Vector3.back,
+                    Quaternion.identity, linesHolder);
             }
         }
 
@@ -32,33 +39,69 @@ public class DrawManager : MonoBehaviour
                 theTrail.transform.position = hit.collider.transform.position + (Vector3)hit.point + Vector3.back;
             }
         }
+        
+        else if (Input.touchCount> 0 && Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetMouseButtonUp(0))
+        {
+            takeSnapshot = true;
+        }
     }
 
     bool takeSnapshot;
-    public void OnClickButton()
-    {
-        takeSnapshot = true;
-    }
+    bool initialSnapshot;
 
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        if (takeSnapshot)
+        if (takeSnapshot || initialSnapshot)
         {
-            var t2d = new Texture2D(src.width, src.width, TextureFormat.RGB24,false);
+            var t2d = new Texture2D(src.width, src.height, TextureFormat.RGB24,false);
             RenderTexture.active = src;
             t2d.ReadPixels(new Rect(0, 0, t2d.width, t2d.height), 0, 0);
             
             var pixels = t2d.GetPixels();
-            var count = 0;
+            var patternCount = 0;
+            var drawCount = 0;
             for (int i = 0; i < pixels.Length; i++)
             {
-                count += (pixels[i].r != pixels[0].r || pixels[i].g != pixels[0].g && pixels[i].b != pixels[0].b) ? 1 : 0;
+                patternCount += (pixels[i].r < 0.1f && pixels[i].g < 0.1f && pixels[i].b < 0.1f) ? 1 : 0;
+                drawCount += (pixels[i].r >0.9f && pixels[i].g < 0.1f && pixels[i].b < 0.1f) ? 1 : 0;
+            }
+
+            if (initialSnapshot)
+            {
+                initialPatternCount = patternCount;
             }
             
-            Debug.Log(count);
+            Debug.Log(patternCount + " "+drawCount);
+            
+
+            var percent = drawCount * lineWeight / initialPatternCount;
+            
+            Debug.Log("result "+ percent);
+
+            float resultValue = 0;
+
+            if (percent <= 1)
+            {
+                resultValue = percent;
+            }
+            else
+            {
+                resultValue = Mathf.Max(0, 2 - percent);
+            }
+
+            result.text = (resultValue * 100).ToString("F2") + "%";
         }
 
         takeSnapshot = false;
+        initialSnapshot = false;
         Graphics.Blit(src, dest);
+    }
+
+    public void OnClearClick()
+    {
+        foreach (Transform child in linesHolder)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
