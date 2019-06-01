bf046b7831b76598e69d7718caa5f9d307db3b8a
daniell <liviudaniel.lupascu@gmail.com>
2019-06-01 17:41:09 +0300

    Initial commit

---
+++ Draw_unity/Assets/Scripts/DrawManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawManager : MonoBehaviour
+{
+    public GameObject drawPrefab;
+
+    public Sprite drawPlane;
+
+    GameObject theTrail;
+
+    public void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0))
+        {
+            var mouseRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var hit = Physics2D.Raycast(mouseRay, Vector2.zero);
+            if (hit.transform != null)
+            {
+                theTrail = Instantiate(drawPrefab, hit.collider.transform.position + (Vector3)hit.point + Vector3.back , Quaternion.identity);
+            }
+        }
+
+        else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetMouseButton(0))
+        {
+            var mouseRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var hit = Physics2D.Raycast(mouseRay, Vector2.zero);
+            if (hit.transform != null)
+            {
+                theTrail.transform.position = hit.collider.transform.position + (Vector3)hit.point + Vector3.back;
+            }
+        }
+    }
+}
