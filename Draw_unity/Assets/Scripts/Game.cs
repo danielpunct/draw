49bbf54458d77a712a14a4b3be6e37516c893194
daniell <liviudaniel.lupascu@gmail.com>
2019-06-03 21:58:32 +0300

    new graphics. svg images'

--- Draw_unity/Assets/Scripts/Game.cs
+++ Draw_unity/Assets/Scripts/Game.cs
@@ -1,8 +1,10 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
-public class DrawManager : MonoBehaviour
+public class Game : MonoBehaviour
 {
+    public RenderTexture activeRT;
     public GameObject drawPrefab;
     public TMP_Text result;
 
@@ -12,9 +14,10 @@ public class DrawManager : MonoBehaviour
     public float lineWeight = 0.22f;
 
     public Transform linesHolder;
-    void Start()
+    IEnumerator Start()
     {
-        initialSnapshot = true;
+        yield return  new WaitForSeconds(1);
+       SampleTexture(true); 
     }
 
     public void Update()
@@ -25,8 +28,9 @@ public class DrawManager : MonoBehaviour
             var hit = Physics2D.Raycast(mouseRay, Vector2.zero);
             if (hit.transform != null)
             {
-                theTrail = Instantiate(drawPrefab, hit.collider.transform.position + (Vector3) hit.point + Vector3.back,
+                theTrail = Instantiate(drawPrefab, (Vector3) hit.point + Vector3.back,
                     Quaternion.identity, linesHolder);
+                theTrail.layer = 8;
             }
         }
 
@@ -36,65 +40,63 @@ public class DrawManager : MonoBehaviour
             var hit = Physics2D.Raycast(mouseRay, Vector2.zero);
             if (hit.transform != null)
             {
-                theTrail.transform.position = hit.collider.transform.position + (Vector3)hit.point + Vector3.back;
+                theTrail.transform.position = (Vector3)hit.point + Vector3.back;
             }
         }
         
         else if (Input.touchCount> 0 && Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetMouseButtonUp(0))
         {
-            takeSnapshot = true;
+           SampleTexture(false); 
         }
     }
 
-    bool takeSnapshot;
-    bool initialSnapshot;
-
-    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    void SampleTexture(bool initialSnapshot)
     {
-        if (takeSnapshot || initialSnapshot)
-        {
-            var t2d = new Texture2D(src.width, src.height, TextureFormat.RGB24,false);
-            RenderTexture.active = src;
+            var t2d = new Texture2D(activeRT.width, activeRT.height, TextureFormat.RGB24, false);
+            RenderTexture.active = activeRT;
             t2d.ReadPixels(new Rect(0, 0, t2d.width, t2d.height), 0, 0);
-            
+
             var pixels = t2d.GetPixels();
             var patternCount = 0;
             var drawCount = 0;
             for (int i = 0; i < pixels.Length; i++)
             {
                 patternCount += (pixels[i].r < 0.1f && pixels[i].g < 0.1f && pixels[i].b < 0.1f) ? 1 : 0;
-                drawCount += (pixels[i].r >0.9f && pixels[i].g < 0.1f && pixels[i].b < 0.1f) ? 1 : 0;
+                drawCount += (pixels[i].r > 0.9f && pixels[i].g < 0.1f && pixels[i].b < 0.1f) ? 1 : 0;
             }
 
             if (initialSnapshot)
             {
                 initialPatternCount = patternCount;
+                Debug.Log("initial " + initialPatternCount +" all: "+ pixels.Length);
+                return;
             }
-            
-            Debug.Log(patternCount + " "+drawCount);
-            
 
-            var percent = drawCount * lineWeight / initialPatternCount;
-            
-            Debug.Log("result "+ percent);
+            Debug.Log(patternCount + " " + drawCount);
+
+            var imagePercent = initialPatternCount / (float) pixels.Length;
+            var putPercent = drawCount * lineWeight / (float) pixels.Length;
+            var drawPercent = 1 - patternCount / (float) initialPatternCount;
+            var extraPercent = (drawCount * lineWeight - initialPatternCount) / pixels.Length;
+
+            Debug.Log("image " + imagePercent + " put " + putPercent + " |   draw " + drawPercent + "   extra: " +
+                      extraPercent);
 
             float resultValue = 0;
 
-            if (percent <= 1)
+            if (putPercent <= 1)
             {
-                resultValue = percent;
+                resultValue = putPercent;
             }
             else
             {
-                resultValue = Mathf.Max(0, 2 - percent);
+                resultValue = Mathf.Max(0, 2 - putPercent);
             }
 
-            result.text = (resultValue * 100).ToString("F2") + "%";
-        }
+            //apply left percent
+            resultValue *= (1 - drawPercent) / putPercent;
 
-        takeSnapshot = false;
-        initialSnapshot = false;
-        Graphics.Blit(src, dest);
+            result.text = (drawPercent * 100).ToString("F2") + "%";
     }
 
     public void OnClearClick()
